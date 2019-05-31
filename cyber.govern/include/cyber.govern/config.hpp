c940a29819c3e50f5bf3db9fc4a862c9bf44e439
Unknown <vadimkaynarov@gmail.com>
2019-05-31 14:04:22 +0300

    config::workers_reward_pct fixed

--- cyber.govern/include/cyber.govern/config.hpp
+++ cyber.govern/include/cyber.govern/config.hpp
@@ -29,7 +29,7 @@ static constexpr auto emission_max_arg = 75 * _1percent;
 //linear interpolation between these two points
 
 static constexpr auto block_reward_pct     = 10 * _1percent;
-static constexpr auto workers_reward_pct   = 20 * _1percent; // not including block reward //TODO: 22 * _1percent
+static constexpr auto workers_reward_pct   = 2222 * _1percent / 100; // not including block reward
 
 }
 
